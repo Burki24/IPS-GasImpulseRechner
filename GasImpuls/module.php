693fb694eaad60b1d18c3305660ff62d717ba5e3
Burki24 <burkhard@kneiseler.de>
2023-03-10 07:03:37 +0100

    Update module.php

--- GasImpuls/module.php
+++ GasImpuls/module.php
@@ -231,8 +231,9 @@
 			$kwh = $calorificValue * $cubicMeter;
 			$this->SendDebug("cubicmeter", $cubicMeter, 0);
 			$this->SendDebug("kwh calculate", $kwh, 0);
-        	return $kwh;
 			$this->SetValue("GCM_UsedKWH", $kwh);}
+        	return $kwh;
+
 		private function getCurrentDate() {
 			$date = date('Y-m-d');
 			list($year, $month, $day) = explode('-', $date);

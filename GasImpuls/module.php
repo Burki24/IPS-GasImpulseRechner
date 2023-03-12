6122fe0c26279a358c79a883b0d565487363d914
Burki24 <burkhard@kneiseler.de>
2023-03-12 09:49:26 +0100

    Update module.php

--- GasImpuls/module.php
+++ GasImpuls/module.php
@@ -36,9 +36,9 @@
 			$this->RegisterAttributeBoolean('Attrib_ImpulseState', 0);
 
 			// Profil erstellen
-			// if (!IPS_VariableProfileExists('GCM.Gas.kWh')) {
+			if (!IPS_VariableProfileExists('GCM.Gas.kWh')) {
 			$this->RegisterProfileFloat('GCM.Gas.kWh', 'Flame', 0, ' kWh', 0, 0, 0, 2);
-			// }
+			}
 
 			// Variablen erstellen
 			$this->RegisterVariableFloat("GCM_UsedKWH", $this->Translate('Daily Cosnumption kW/h'), "GCM.Gas.kWh");

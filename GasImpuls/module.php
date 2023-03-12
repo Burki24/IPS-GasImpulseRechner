f3c1acf0b5c32dd8964bc2f7ffc75af57b5be3db
Burki24 <burkhard@kneiseler.de>
2023-03-12 09:54:39 +0100

    Update module.php

--- GasImpuls/module.php
+++ GasImpuls/module.php
@@ -115,6 +115,7 @@
 						$this->calculateKWH($calorificValue, $cubicMeter);
 						$this->CostActualDay();
 						$this->Difference();
+						$this->DayCounter();
 					break;
                 default:
                     $this->SendDebug(__FUNCTION__ . ':: Messages from Sender ' . $SenderID, $Data, 0);

a22baa7c53f1cc37d27363dd9ac21c9df64fab72
Burki24 <burkhard@kneiseler.de>
2023-03-14 17:13:00 +0100

    Update module.php

--- GasImpuls/module.php
+++ GasImpuls/module.php
@@ -88,12 +88,13 @@
             $oldCounterValue = $this->ReadAttributeFloat('Attrib_InstallCounterValueOld');
             $newCounterValue = $this->ReadPropertyFloat('InstallCounterValue');
             $newDayCount = $this->ReadPropertyFloat('InstallDayCount');
+            $this->SendDebug('Install Day Count', $newDayCount, 0);
             if ($oldCounterValue !== $newCounterValue) {
                 $this->SetValue('GCM_UsedM3', $this->ReadPropertyFloat('InstallDayCount'));
                 $this->SendDebug('GCM_USedM3', $this->GetValue('GCM_UsedM3'), 0);
                 $this->WriteAttributeFloat('Attrib_InstallCounterValueOld', $this->ReadPropertyFloat('InstallCounterValue'));
                 $this->WriteAttributeFloat('Attrib_CounterValue', 0);
-                $this->WriteAttributeFloat('Attrib_UsedM3', $newDayCount);
+                // $this->SetValue('GCM_UsedM3', $newDayCount);
             }
             // Event Tagesende starten
             $this->RegisterEvent();

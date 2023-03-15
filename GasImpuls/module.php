e1ace4c6eec4a9f4bf9f47ff1535a0fb9101bb11
Burki24 <burkhard@kneiseler.de>
2023-03-15 10:04:57 +0100

    Update module.php

--- GasImpuls/module.php
+++ GasImpuls/module.php
@@ -313,9 +313,9 @@
             $newDayCount = $this->ReadPropertyFloat('InstallDayCount');
             $this->SendDebug('Install Day Count', $newDayCount, 0);
             $this->SetValue('GCM_UsedM3', $this->ReadPropertyFloat('InstallDayCount'));
-                $this->SendDebug('GCM_USedM3', $this->GetValue('GCM_UsedM3'), 0);
-                $this->WriteAttributeFloat('Attrib_InstallCounterValueOld', $this->ReadPropertyFloat('InstallCounterValue'));
-                $this->WriteAttributeFloat('Attrib_CounterValue', 0);
-                $this->SetValue('GCM_UsedM3', $newDayCount);
+            $this->SendDebug('GCM_USedM3', $this->GetValue('GCM_UsedM3'), 0);
+            $this->WriteAttributeFloat('Attrib_InstallCounterValueOld', $this->ReadPropertyFloat('InstallCounterValue'));
+            $this->WriteAttributeFloat('Attrib_CounterValue', 0);
+            $this->SetValue('GCM_UsedM3', $newDayCount);
         }
     }

0798c998e06f51f894ae74bee76c23b211b63e0e
Burki24 <burkhard@kneiseler.de>
2023-03-19 08:23:26 +0100

    Update module.php

--- GasImpuls/module.php
+++ GasImpuls/module.php
@@ -187,7 +187,7 @@
             $impulseAttrib = $this->ReadAttributeBoolean('Attrib_ImpulseState');
             $this->SendDebug('Impulse Status GasCounter', $impulseAttrib, 0);
             $counterValue = $this->ReadAttributeFloat('Attrib_ActualCounterValue');
-            $this->SendDebug('Attribute actual CounterValue -  GasCounter', $impulseAttrib, 0);
+            $this->SendDebug('Attribute actual CounterValue -  GasCounter', $counterValue, 0);
             $cubicMeter = $this->GetValue('GCM_UsedM3');
             $installCounterValue = $this->ReadPropertyFloat('InstallCounterValue');
             $currentCounterValue = $this->GetValue('GCM_CounterValue');

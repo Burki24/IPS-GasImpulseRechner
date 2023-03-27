91098a68d879c8f5dfe2ee3430c48989bc66c8b7
Burki24 <burkhard@kneiseler.de>
2023-03-27 08:40:38 +0200

    Update module.php

--- GasImpuls/module.php
+++ GasImpuls/module.php
@@ -153,7 +153,7 @@
         private function InstallCounterValue()
         {
             $instanceID = $_IPS['TARGET'];
-            $instanceID1 = IPS_GetObjectIDByIdent('InstallCounterValue', $instanceID);
+            $instanceID1 = $this->GetValue('InstallCounterValue');
             $this->SendDebug('ID des Moduls', $instanceID, 0);
             $this->SendDebug('ID des Property', $instanceID1, 0);
             if (IPS_GetObjectIDByIdent('InstallCounterValue', $instanceID) > 0) {

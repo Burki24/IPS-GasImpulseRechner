ebe5621b792d6ad14cf3e12276dc0725028a0603
Burki24 <burkhard@kneiseler.de>
2023-03-30 06:34:20 +0200

    Update module.php

--- GasImpuls/module.php
+++ GasImpuls/module.php
@@ -23,7 +23,7 @@
             $this->RegisterPropertyString('InvoiceDate', $this->GetCurrentDate());
             $this->RegisterPropertyFloat('InstallCounterValue', 0);
             $this->RegisterPropertyFloat('KWHPrice', 0);
-            $this->RegisterPropertyInteger('BillingMonths', '11');
+            $this->RegisterPropertyInteger('BillingMonths', 11);
                         $this->SendDebug('Anzahl Monate', $this->ReadPropertyInteger('BillingMonths'), 0);
 
             // Zur Berechnung bereitzustellende Werte

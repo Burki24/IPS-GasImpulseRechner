6ff22594c52f44362a648316492c0d88fdc7669f
Burki24 <burkhard@kneiseler.de>
2023-04-04 10:10:58 +0200

    Update CalculationHelper.php

--- libs/CalculationHelper.php
+++ libs/CalculationHelper.php
@@ -137,6 +137,7 @@ trait CalculationHelper
     // KWH Forecast
     private function InvoiceKWH($invoice_kwh, $invoice_date, $kwh)
     {
+                $this - SendDebug('Invoice_date', $invoice_date, 0);
         $days_in_year = (int) date('L') ? 366 : 365;
         $date = json_decode($invoice_date, true);
         $time_stamp = mktime(0, 0, 0, $date['month'], $date['day'], $date['year']);

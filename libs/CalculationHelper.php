bcf6baf48fe289615aed42db054be626c8e8c791
Burki24 <burkhard@kneiseler.de>
2023-04-08 07:06:34 +0200

    Update CalculationHelper.php

--- libs/CalculationHelper.php
+++ libs/CalculationHelper.php
@@ -167,7 +167,7 @@ trait CalculationHelper
         $this->SendDebug('Jahr', $current_year, 0);
         if (is_int($current_year) && is_numeric($current_month)) {
             foreach ($weights as $month => $weight) {
-                $days_in_month = cal_days_in_month(CAL_GREGORIAN, intval(date('m', strtotime("1 $current_year-$month"))), $current_year);
+                $days_in_month = cal_days_in_month(CAL_GREGORIAN, intval(date('m', strtotime("1 {$current_year}-{$month}"))), $current_year);
                 $monthly_sum = 0;
                 $output = '';
                 if ($days_in_month === false) {

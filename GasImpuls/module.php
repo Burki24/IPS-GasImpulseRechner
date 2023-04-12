256b8be45bbe64744d1fde5de0bafad6d4d30ac8
Burki24 <burkhard@kneiseler.de>
2023-04-12 12:04:24 +0200

    Update module.php

--- GasImpuls/module.php
+++ GasImpuls/module.php
@@ -273,13 +273,13 @@
                 if ($impulse) {
                     $new_counter_value = $current_counter_value + $impulse_value;
                     $new_cubic_meter = $cubic_meter + $impulse_value;
-                // $this->calculations($base_price, $invoice_date, $calorific_value, $current_consumption, $kwh_price);
-                    // $this->calculateKWH($calorific_value, $cubic_meter);
-                    // $this->CalculateCostActualDay($base_price, $calorific_value, $kwh_day, $kwh_price);
-                    // $this->DifferenceFromInvoice($actual_counter_value, $invoice_count, $calorific_value);
-                    // $this->LumpSumDifference($lump_sum_year, $costs_forecast);
-                    // $this->ForecastKWH($invoice_kwh, $invoice_date, $actual_kwh, $month_factor);
-                    // $this->SendDebug('Modul.php -> IF actual KWH', $actual_kwh, 0);
+                    $this->calculations($base_price, $invoice_date, $calorific_value, $current_consumption, $kwh_price);
+                    $this->calculateKWH($calorific_value, $cubic_meter);
+                    $this->CalculateCostActualDay($base_price, $calorific_value, $kwh_day, $kwh_price);
+                    $this->DifferenceFromInvoice($actual_counter_value, $invoice_count, $calorific_value);
+                    $this->LumpSumDifference($lump_sum_year, $costs_forecast);
+                    $this->ForecastKWH($invoice_kwh, $invoice_date, $actual_kwh, $month_factor);
+                    $this->SendDebug('Modul.php -> IF actual KWH', $actual_kwh, 0);
                 } else {
                     $new_counter_value = $current_counter_value;
                     $new_cubic_meter = $cubic_meter;

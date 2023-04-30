f5947dbd84a938f17494d08771ec8a294c4768c3
Burki24 <burkhard@kneiseler.de>
2023-04-30 08:00:58 +0200

    Update CalculationHelper.php

--- libs/CalculationHelper.php
+++ libs/CalculationHelper.php
@@ -93,7 +93,7 @@ trait CalculationHelper
     }
 
     // Berechnung Differenz zwischen m3 Rechnungsstellung und Aktuell
-    private function DifferenceFromInvoice(float $actual_counter_value, float $invoice_count, float $calorific_value, float $condition_number): float
+    private function DifferenceFromInvoice(float $actual_counter_value, float $invoice_count, float $calorific_value, float $condition_number): array
     {
         $result = ($actual_counter_value - $invoice_count);
         $kwh = ($result * $calorific_value * $condition_number);

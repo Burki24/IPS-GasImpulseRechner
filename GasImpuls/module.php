b14b8474b323cae0bf97d87b02302aa22adb00c9
Burkhard Kneiseler <burkhard@kneiseler.de>
2023-05-11 05:06:05 +0200

    Update module.php

--- GasImpuls/module.php
+++ GasImpuls/module.php
@@ -3,7 +3,6 @@
     declare(strict_types=1);
 
     require_once __DIR__ . '/../libs/CalculationHelper.php';
-    // require_once __DIR__ . '/../libs/SymconModulHelper/DebugHelper.php';
 
     class GasImpulsVerbrauchsanalyse extends IPSModule
     {
